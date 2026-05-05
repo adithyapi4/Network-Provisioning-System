6f7782ead98b0c104c7c22b567d7f21e6b490ea1
Adithya C S <135451003+adithyapi4@users.noreply.github.com>
2026-05-05 19:03:49 +0530

    Added exception handling

--- main.cpp
+++ main.cpp
@@ -23,180 +23,187 @@ int main()
         std::cin >> option;
         if (option == 3)
             return 0;
-        switch (option)
+        try
         {
-        case 1:
-            std::cout << "Choose a connection type: \n";
-            std::cout << "1. Prepaid\n";
-            std::cout << "2. Postpaid\n";
-            std::cin >> option;
-
             switch (option)
             {
             case 1:
-                std::cout << "1. New Connection\n";
-                std::cout << "2. Port In\n";
+                std::cout << "Choose a connection type: \n";
+                std::cout << "1. Prepaid\n";
+                std::cout << "2. Postpaid\n";
                 std::cin >> option;
 
                 switch (option)
                 {
                 case 1:
-
-                    std::cout << "Enter the following details: " << std::endl;
-                    std::cout << "Name as per Govt ID.: ";
-                    std::getline(std::cin >> std::ws, name);
-                    std::cout << "Pincode: ";
-                    std::cin >> pincode;
-                    std::cout << "Aadhaar Number: ";
-                    std::cin >> aadhaar;
-                    std::cout << "Email Address: ";
-                    std::cin >> email;
-                    mobile_connections.emplace_back(name, pincode, aadhaar, email, "PR");
-                    std::cout << "Connection request submitted successfully"; << std::endl;
-                    break;
-
-                case 2:
-
-                    std::cout << "Enter the following details: " << std::endl;
-                    std::cout << "Name as per Govt ID.: ";
-                    std::getline(std::cin >> std::ws, name);
-                    std::cout << "Pincode: ";
-                    std::cin >> pincode;
-                    std::cout << "Aadhaar Number: ";
-                    std::cin >> aadhaar;
-                    std::cout << "Email Address: ";
-                    std::cin >> email;
-                    std::cout << "Enter your phone number: ";
-                    std::cin >> phone_no;
-                    mobile_connections.emplace_back(name, pincode, aadhaar, email, "PR", phone_no);
-                    std::cout << "Connection request submitted successfully" << std::endl;
-                    break;
-
-                default:
-
-                    std::cout << "Invalid Option" << std::endl;
-                }
-                break;
-
-            case 2:
-                std::cout << "1. New Connection\n";
-                std::cout << "2. Port In\n";
-                std::cin >> option;
-
-                switch (option)
-                {
-                case 1:
-
-                    std::cout << "Enter the following details: " << std::endl;
-                    std::cout << "Name as per Govt ID.: ";
-                    std::getline(std::cin >> std::ws, name);
-                    std::cout << "Pincode: ";
-                    std::cin >> pincode;
-                    std::cout << "Aadhaar Number: ";
-                    std::cin >> aadhaar;
-                    std::cout << "Email Address: ";
-                    std::cin >> email;
-                    mobile_connections.emplace_back(name, pincode, aadhaar, email, "PO");
-                    std::cout << "Connection request submitted successfully" << std::endl;
-                    break;
-
-                case 2:
-
-                    std::cout << "Enter the following details: " << std::endl;
-                    std::cout << "Name as per Govt ID.: ";
-                    std::getline(std::cin >> std::ws, name);
-                    std::cout << "Pincode: ";
-                    std::cin >> pincode;
-                    std::cout << "Aadhaar Number: ";
-                    std::cin >> aadhaar;
-                    std::cout << "Email Address: ";
-                    std::cin >> email;
-                    std::cout << "Enter your phone number: ";
-                    std::cin >> phone_no;
-                    mobile_connections.emplace_back(name, pincode, aadhaar, email, "PO", phone_no);
-                    std::cout << "Connection request submitted successfully" << std::endl;
-                    break;
-
-                default:
-
-                    std::cout << "Invalid option!" << std::endl;
-                    break;
-                }
-                break;
-
-            default:
-                std::cout << "Invalid option!" << std::endl;
-            }
-            break;
-
-        case 2:
-            if (verify_prov_id() == 0)
-            {
-                while (1)
-                {
-                PROVISION_MENU:
-                    std::cout << "1. View Pending Requests\n";
-                    std::cout << "2. View Rejected Requests\n";
-                    std::cout << "3. View Provisioned Requests\n";
-                    std::cout << "4.Exit\n";
+                    std::cout << "1. New Connection\n";
+                    std::cout << "2. Port In\n";
                     std::cin >> option;
-
-                    if (option == 4)
-                    {
-                        return 0;
-                    }
 
                     switch (option)
                     {
                     case 1:
-                        for (const Mobile &connection : mobile_connections)
-                        {
-                            if (connection.get_status() == "Pending")
-                            {
-                                std::cout<<connection;
-                            }
-                        }
-                        std::cout << "Enter the CRN you want to provision: ";
-                        std::cin >> crn;
-                        for (Mobile &connection : mobile_connections)
-                        {
-                            if (connection.get_crn() == crn)
-                            {
-                                mobile_provisioning(connection);
-                                goto PROVISION_MENU;
-                            }
-                        }
-                        std::cout << "Invalid CRN!"<<std::endl;
+
+                        std::cout << "Enter the following details: " << std::endl;
+                        std::cout << "Name as per Govt ID.: ";
+                        std::getline(std::cin >> std::ws, name);
+                        std::cout << "Pincode: ";
+                        std::cin >> pincode;
+                        std::cout << "Aadhaar Number: ";
+                        std::cin >> aadhaar;
+                        std::cout << "Email Address: ";
+                        std::cin >> email;
+                        mobile_connections.emplace_back(name, pincode, aadhaar, email, "PR");
+                        std::cout << "Connection request submitted successfully" << std::endl;
                         break;
 
                     case 2:
-                        for (const Mobile &connection : mobile_connections)
-                        {
-                            if (connection.get_status() == "Rejected")
-                            {
-                                std::cout<<connection;
-                            }
-                        }
-                        break;
 
-                    case 3:
-                        for (const Mobile &connection : mobile_connections)
-                        {
-                            if (connection.get_status() == "Provisioned")
-                            {
-                                std::cout<<connection;
-                            }
-                        }
+                        std::cout << "Enter the following details: " << std::endl;
+                        std::cout << "Name as per Govt ID.: ";
+                        std::getline(std::cin >> std::ws, name);
+                        std::cout << "Pincode: ";
+                        std::cin >> pincode;
+                        std::cout << "Aadhaar Number: ";
+                        std::cin >> aadhaar;
+                        std::cout << "Email Address: ";
+                        std::cin >> email;
+                        std::cout << "Enter your phone number: ";
+                        std::cin >> phone_no;
+                        mobile_connections.emplace_back(name, pincode, aadhaar, email, "PR", phone_no);
+                        std::cout << "Connection request submitted successfully" << std::endl;
                         break;
 
                     default:
+
+                        std::cout << "Invalid Option" << std::endl;
+                    }
+                    break;
+
+                case 2:
+                    std::cout << "1. New Connection\n";
+                    std::cout << "2. Port In\n";
+                    std::cin >> option;
+
+                    switch (option)
+                    {
+                    case 1:
+
+                        std::cout << "Enter the following details: " << std::endl;
+                        std::cout << "Name as per Govt ID.: ";
+                        std::getline(std::cin >> std::ws, name);
+                        std::cout << "Pincode: ";
+                        std::cin >> pincode;
+                        std::cout << "Aadhaar Number: ";
+                        std::cin >> aadhaar;
+                        std::cout << "Email Address: ";
+                        std::cin >> email;
+                        mobile_connections.emplace_back(name, pincode, aadhaar, email, "PO");
+                        std::cout << "Connection request submitted successfully" << std::endl;
+                        break;
+
+                    case 2:
+
+                        std::cout << "Enter the following details: " << std::endl;
+                        std::cout << "Name as per Govt ID.: ";
+                        std::getline(std::cin >> std::ws, name);
+                        std::cout << "Pincode: ";
+                        std::cin >> pincode;
+                        std::cout << "Aadhaar Number: ";
+                        std::cin >> aadhaar;
+                        std::cout << "Email Address: ";
+                        std::cin >> email;
+                        std::cout << "Enter your phone number: ";
+                        std::cin >> phone_no;
+                        mobile_connections.emplace_back(name, pincode, aadhaar, email, "PO", phone_no);
+                        std::cout << "Connection request submitted successfully" << std::endl;
+                        break;
+
+                    default:
+
+                        std::cout << "Invalid option!" << std::endl;
                         break;
                     }
-                }
-            }
+                    break;
 
-        default:
-            std::cout << "Invalid choice";
+                default:
+                    std::cout << "Invalid option!" << std::endl;
+                }
+                break;
+
+            case 2:
+                if (verify_prov_id() == 0)
+                {
+                    while (1)
+                    {
+                    PROVISION_MENU:
+                        std::cout << "1. View Pending Requests\n";
+                        std::cout << "2. View Rejected Requests\n";
+                        std::cout << "3. View Provisioned Requests\n";
+                        std::cout << "4.Exit\n";
+                        std::cin >> option;
+
+                        if (option == 4)
+                        {
+                            return 0;
+                        }
+
+                        switch (option)
+                        {
+                        case 1:
+                            for (const Mobile &connection : mobile_connections)
+                            {
+                                if (connection.get_status() == "Pending")
+                                {
+                                    std::cout << connection;
+                                }
+                            }
+                            std::cout << "Enter the CRN you want to provision: ";
+                            std::cin >> crn;
+                            for (Mobile &connection : mobile_connections)
+                            {
+                                if (connection.get_crn() == crn)
+                                {
+                                    mobile_provisioning(connection);
+                                    goto PROVISION_MENU;
+                                }
+                            }
+                            std::cout << "Invalid CRN!" << std::endl;
+                            break;
+
+                        case 2:
+                            for (const Mobile &connection : mobile_connections)
+                            {
+                                if (connection.get_status() == "Rejected")
+                                {
+                                    std::cout << connection;
+                                }
+                            }
+                            break;
+
+                        case 3:
+                            for (const Mobile &connection : mobile_connections)
+                            {
+                                if (connection.get_status() == "Provisioned")
+                                {
+                                    std::cout << connection;
+                                }
+                            }
+                            break;
+
+                        default:
+                            break;
+                        }
+                    }
+                }
+
+            default:
+                std::cout << "Invalid choice";
+            }
+        }
+        catch (const char *msg)
+        {
+            std::cout << msg << std::endl;
         }
     }
     return 0;
@@ -212,7 +219,7 @@ int verify_prov_id()
     std::cout << "Enter the OTP sent to your registered mobile number: ";
     std::cin >> otp;
 
-    std::cout << "Validated"<<std::endl;
+    std::cout << "Validated" << std::endl;
     return 0;
 }
 
@@ -232,19 +239,19 @@ void mobile_provisioning(Mobile &connection)
     connection.status = "Provisioned";
 }
 
-std::ostream& operator << (std::ostream& out,const Mobile& sr)
+std::ostream &operator<<(std::ostream &out, const Mobile &sr)
 {
-    if(sr.status=="Pending")
+    if (sr.status == "Pending")
     {
-        out<<sr.crn<<std::endl;
+        out << sr.crn << std::endl;
     }
-    else if(sr.status=="Rejected")
+    else if (sr.status == "Rejected")
     {
-        out<<sr.crn<<"\t\t"<<sr.reason<<std::endl;
+        out << sr.crn << "\t\t" << sr.reason << std::endl;
     }
-    else if(sr.status=="Provisioned")
+    else if (sr.status == "Provisioned")
     {
-        std::cout<<sr.crn<<"\t\t"<<sr.iccid<<std::endl;
+        std::cout << sr.crn << "\t\t" << sr.iccid << std::endl;
     }
 
     return out;
