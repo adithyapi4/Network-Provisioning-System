000fd1df45bc0f1646daa818a9c2c44e3ea9bed7
Adithya C S <135451003+adithyapi4@users.noreply.github.com>
2026-05-05 19:01:15 +0530

    Fixed formatting and spacing

--- Customer.cpp
+++ Customer.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "Customer.h"
 
-Customer::Customer(std::string name,unsigned int pincode,std::string aadhaar_no,std::string email) : name(name),pincode(pincode),aadhaar_no(aadhaar_no),email(email)
+Customer::Customer(std::string name, unsigned int pincode, std::string aadhaar_no, std::string email) : name(name), pincode(pincode), aadhaar_no(aadhaar_no), email(email)
 {
     verify_email();
 }
 
 void Customer::verify_email()
 {
-    std::cout<<"Enter the OTP sent to "<<email<<": ";
-    std::cin>>otp;
-    std::cout<<"Verified Successfully"<<std::endl;
+    std::cout << "Enter the OTP sent to " << email << ": ";
+    std::cin >> otp;
+    std::cout << "Verified Successfully" << std::endl;
 }
